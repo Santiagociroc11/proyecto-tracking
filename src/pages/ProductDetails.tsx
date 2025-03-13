6230054a9fd90f255ad422e1dad0a4357b3f4c8b
Santiagociroc11 <santiagociiro12@gmail.com>
2025-03-13 18:52:29 -0500

    amplia

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -234,7 +234,7 @@ fbq('track', 'PageView');
   }
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <div className="mb-6 flex items-center justify-between">
         <Link
           to="/"
